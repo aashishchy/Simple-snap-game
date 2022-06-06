8d955bc45fd9cc099d138978da0c939e7d195b57
Aashish Chaudhary <67904314+aashishchy@users.noreply.github.com>
2022-06-06 11:52:45 +0100

    Add files via upload

---
+++ simple snap/snap.py
@@ -0,0 +1,124 @@
+
+from random import shuffle #importing shuffle from random module
+
+#card class has two variable
+class Card:
+    suits = ["spades",
+             "hearts",
+             "diamonds",
+             "clubs"]
+
+    values = [None, None,"2", "3",
+              "4", "5", "6", "7",
+              "8", "9", "10",
+              "Jack", "Queen",
+              "King", "Ace"]
+
+    def __init__(self, v, s):
+        """suit + value are ints"""
+        self.value = v
+        self.suit = s
+
+    def __lt__(self, c2):
+        if self.value < c2.value:
+            return True
+        if self.value == c2.value:
+            if self.suit < c2.suit:
+                return True
+            else:
+                return False
+        return False
+
+    def __gt__(self, c2):
+        if self.value > c2.value:
+            return True
+        if self.value == c2.value:
+            if self.suit > c2.suit:
+                return True
+            else:
+                return False
+        return False
+
+    def __repr__(self):
+        v = self.values[self.value] +\
+            " of " + \
+            self.suits[self.suit]
+        return v
+
+#deck class to handle
+class Deck:
+    def __init__(self):
+        self.cards = []
+        for i in range(2, 15):
+            for j in range(4):
+                self.cards\
+                    .append(Card(i,
+                                 j))
+        shuffle(self.cards)
+
+    def rm_card(self):
+        if len(self.cards) == 0:
+            return
+        return self.cards.pop()
+
+
+class Player:
+    def __init__(self, name):
+        
+        self.card = None
+        self.name = name
+
+
+class Game:
+    def __init__(self):
+        name1 = ("p1 name ")#assigning name on players
+        name2 = ("p2 name ")
+        self.deck = Deck()
+        self.p1 = Player(name1)
+        self.p2 = Player(name2)
+
+
+    def draw(self, p1n, p1c, p2n, p2c):
+        d = "{} drew {} {} drew {}"
+        d = d.format(p1n,
+                     p1c,
+                     p2n,
+                     p2c)
+        print(d)
+
+    def play_game(self):
+        cards = self.deck.cards
+        print("beginning War!")
+        #check if card are equal or not
+        while len(cards) >= 2:
+            
+            p1c = self.deck.rm_card()
+            p2c = self.deck.rm_card()
+            p1n = self.p1.name
+            p2n = self.p2.name
+            self.draw(p1n,
+                      p1c,
+                      p2n,
+                      p2c)
+            if p1c > p2c:  
+                continue
+               
+            elif p1c<p2c:
+                continue
+                
+            elif p1c==p2c:   #if two card are equal the it will break loop
+                print("SNAP!")
+                break
+            else:
+                print("SNAP!")
+
+
+       
+        
+        print("WAR is over."+\
+            " ITs a SNAP")
+
+
+game = Game()
+game.play_game()
+    
